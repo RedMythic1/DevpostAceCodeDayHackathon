4ea1ece26868d1e7fcfc6d9fa0087fe0cc023004
RedMythic <ab7552@pleasantonusd.net>
2023-05-13 19:06:00 -0700

    Co-authored-by: DatDerpyDude81 <DatDerpyDude81@users.noreply.github.com> Co-authored-by: chronosirius <chronosirius@users.noreply.github.com>

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ api4 = "fctEUhj7dH4ryjYVX"
 openai.api_key = api1+api2+api3+api4
 
 type_of_question = input("What do you want your questions to be about (e.g. Geometry, Geography, Math, Science, English)?\n")
-quantity = 15
+quantity = 10 mhm big balls
 x=0
 questions = []
 while x<int(quantity):
@@ -18,10 +18,13 @@ while x<int(quantity):
         model='text-davinci-003',
         prompt=f'Make a test question about {type_of_question}',
         max_tokens=100,
-        temperature=0.4588927566627376460023478221588477626657485961914062523232322498279485738628735639705
+        temperature=0.45889275666273764600234782215884776266574859619140625232323224982794857386287356397041
     )
     questions.append(question['choices'][0]['text'].replace('\n','<br>'))
     x+=1
     
 print(questions)
     
+    
+app = flask.Flask(__name__)
+
