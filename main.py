1c64adb5d4d16d56aa6004ae1e3e0adc2599c1fa
RedMythic <ab7552@pleasantonusd.net>
2023-05-13 17:36:18 -0700

    Co-authored-by: arush.gajula@gmail.com <arush.gajula@gmail.com> Co-authored-by: DatDerpyDude81 <DatDerpyDude81@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ api3 = "vlfYodT3BlbkFJkMz"
 api4 = "fctEUhj7dH4ryjYVX"
 
 openai.api_key = api1+api2+api3+api4
+
+print(openai.Completion.create(
+  model="ft-g6dcs8KWwliZ77Bq3zgOlfsB",
+  prompt="is '10', the right answer to: '9+1'",
+  max_tokens=7,
+  temperature=0
+))
