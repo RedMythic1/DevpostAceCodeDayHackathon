7247066bb9906c36539913669065cddaf13f7633
RedMythic <ab7552@pleasantonusd.net>
2023-05-13 17:55:21 -0700

    Co-authored-by: DatDerpyDude81 <DatDerpyDude81@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ api2 ="tsozY7yFhMibo1"
 api3 = "vlfYodT3BlbkFJkMz"
 api4 = "fctEUhj7dH4ryjYVX"
 
+print("hi")
+
 openai.api_key = api1+api2+api3+api4
 
-print(openai.Completion.create(
-  model="ft-g6dcs8KWwliZ77Bq3zgOlfsB",
-  prompt="is '10', the right answer to: '9+1'",
-  max_tokens=7,
-  temperature=0
-))
+openai.FineTune.create(training_file="file-IlRHoO5W8RG7MDudmuVNfd0Z")
