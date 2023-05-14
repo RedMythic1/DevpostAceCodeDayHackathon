d569d6084c31bc238df13b1b7975c9ebce25bb02
RedMythic <ab7552@pleasantonusd.net>
2023-05-13 18:23:13 -0700

    j

--- main.py
+++ main.py
@@ -9,4 +9,19 @@ print("hi")
 
 openai.api_key = api1+api2+api3+api4
 
-type = 
+type_of_question = input("What do you want your questions to be about (e.g. Geometry, Geography, Math, Science, English)?\n")
+quantity = input("How many questions do you want\n")
+x=0
+questions = []
+while x<quantity:
+    question = openai.Completion.create(
+        model='text-davinci-003',
+        prompt=f'Make a test question about {type_of_question}',
+        max_tokens=100,
+        temperature=0.4588927566627376460023478221588477626657485961914062523232322498279485738628735639705
+    )
+    questions.append(question)
+    x+=1
+    
+print(questions)
+    
